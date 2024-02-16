768f5fbcb16d6fd6ffcef364003cc18a7909c85a
Sam <dexxrolo@gmail.com>
2024-02-16 16:16:07 +0530

    Add: respond to regex based query

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
 
 const app = new App({
@@ -6,15 +5,36 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// All the room in the world for your code
+// Regular expression for matching specific message format
+const helpTextFormat =
+  /Component:\s*([^:\n]+)\s*\nIssue-Type:\s*([^:\n]+)\s*\nPriority:\s*([^:\n]+)\s*\nVersion:\s*([^:\n]+)\s*\nDescription:\s*([^:\n]+)\s*\nScreenshot\/Video:\s*([^:\n]+)/;
 
+// Start the app
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
 
+  // Listen for a specific text message and respond in the same thread
   app.message("hola amigo", async ({ message, say }) => {
-    console.log(message);
+    console.log(message, "replying as a new message");
     await say(`kaise ho? theek ho? <@${message.user}>`);
+  });
+
+  // Listen for a specific text message ("hello") and respond in the same thread
+  app.message("hello", async ({ message, say }) => {
+    console.log(message, "replying in thread");
+    await say({
+      text: `Hello <@${message.user}>!`,
+      thread_ts: message.ts,
+    });
+  });
+
+  // Listen for messages that match the specified format and respond in the same thread
+  app.message(helpTextFormat, async ({ message, say }) => {
+    console.log(message, "replying in thread of help query");
+    await say({
+      text: `Hello <@${message.user}>!`,
+      thread_ts: message.ts,
+    });
   });
 
   console.log(`⚡️ Bolt app is running on ${process.env.PORT || 3000}`);
